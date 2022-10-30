22d4e8564476d22dc478d41bc4e51fdb2bc36e34
kdeary <korbin.deary45@gmail.com>
2022-10-30 14:53:28 -0400

    user context

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, send_from_directory, render_template, url_for
 from flask_autoindex import AutoIndexBlueprint
 from flask import Blueprint
-from flask_login import LoginManager, logout_user
+from flask_login import LoginManager, logout_user, current_user
 from flask_login import login_user
 import os
 from flask import request
@@ -22,65 +22,67 @@ app.register_blueprint(bp_ai, url_prefix='/files')
 
 #User management
 class User:
-    name = ""
-    def __init__(self, name):
-        self.name = name
-    def is_authenticated(self):
-        return True
-    def is_active(self):
-        return True
-    def is_anonymous(self):
-        return False
-    def get_id(self):
-        return self.name
-    def to_json(self):
-        return {"name": self.name}
+	name = ""
+	def __init__(self, name):
+		self.name = name
+	def is_authenticated(self):
+		return True
+	def is_active(self):
+		return True
+	def is_anonymous(self):
+		return False
+	def get_id(self):
+		return self.name
+	def to_json(self):
+		return {"name": self.name}
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
-    return User(user_id)
+	return User(user_id)
 
 
 @app.route('/login', methods = ['POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
-    if username == 'admin' and password == 'admin':
-        user = User(username)
-        login_user(user)
-        return redirect('/')
-    else:
-        return redirect('/login?error=1')
+	username = request.form['username']
+	password = request.form['password']
+	if username == 'admin' and password == 'admin':
+		user = User(username)
+		login_user(user)
+		return redirect('/')
+	else:
+		return redirect('/login?error=1')
+
 @app.route('/logout')
 def logout():
-    logout_user()
-    return redirect('/')
+	logout_user()
+	return redirect('/')
+
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('home.html', user=current_user)
 
 @app.route('/contact_us')
 def contact_us():
-	return render_template('contact_us.html')
+	return render_template('contact_us.html', user=current_user)
 
 @app.route('/manufacturing')
 def manufacturing():
-	return render_template('manufacturing.html')
+	return render_template('manufacturing.html', user=current_user)
 
 @app.route('/solar_generation')
 def solar_generation():
-	return render_template('solar_generation.html')
+	return render_template('solar_generation.html', user=current_user)
 
 @app.route('/login', methods = ['GET'])
 def login_page():
-	return render_template('login.html')
+	return render_template('login.html', didLoginFail=(request.args.get('error') == '1'), user=current_user)
 
 @app.route('/admin')
 def admin():
-	return render_template('admin.html')
-    
+	return render_template('admin.html', user=current_user)
+	
 if __name__ == "__name__":
-    app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000)
