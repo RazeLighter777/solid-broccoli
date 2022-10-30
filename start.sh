ea3c4ab2ee694a1cd1fc7ff2ee49b28e1d3ade20
Justin Suess <justin.suess@westpoint.edu>
2022-10-30 13:30:26 -0400

    fixing error

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-curlftpfs $SERVER_ADDRESS /mnt -o user=$FTP_USER:$FTP_PASS,allow_other &
-python3 app.py
+curlftpfs $SERVER_ADDRESS /mnt -o user=$FTP_USER:$FTP_PASS,allow_other
+python3 -m flask run --host=0.0.0.0
