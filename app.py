6a7064d65e7b938a4340b7f7b9ab5f43e467614c
kdeary <korbin.deary45@gmail.com>
2022-10-30 13:15:04 -0400

    Added generic info pages

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, send_from_directory, render_template
 
 app = Flask(__name__,
-    static_url_path='', 
-    static_folder='static',
-    template_folder='templates'
+	static_url_path='', 
+	static_folder='static',
+	template_folder='templates'
 )
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/contact_us')
 def contact_us():
-    return render_template('contact_us.html')
+	return render_template('contact_us.html')
+
+@app.route('/manufacturing')
+def manufacturing():
+	return render_template('manufacturing.html')
+
+@app.route('/solar_generation')
+def solar_generation():
+	return render_template('solar_generation.html')
 
 app.run(host='0.0.0.0', port=5000)
