495136fa8db32c854532523aae94f6cbba737f8a
kdeary <korbin.deary45@gmail.com>
2022-11-05 14:41:55 -0400

    solar array data fix

--- solar.py
+++ solar.py
@@ -1,36 +1,44 @@
 from flask import redirect, render_template, request, Blueprint, Response
 from flask_login import LoginManager, login_required, logout_user, current_user, login_user
 import requests
+import socketio
 
 bp = Blueprint('solar', __name__)
 
-def init_bp(app):
-	# @bp.before_request
-	# @login_required
-	# def login_required_for_all_request():    
-	# 	pass
+sio = socketio.Client(logger=False, engineio_logger=False)
 
+solarArrayData = [{},{},{},{},{},{}];
+
+@sio.event
+def connect():
+	print("I'm connected!")
+
+@sio.event
+def connect_error(data):
+	print("The connection failed!")
+
+@sio.event
+def disconnect():
+	print("I'm disconnected!")
+
+sio.connect('http://10.0.122.76:1880', namespaces=['/react_ArrayOV'], socketio_path='uibuilder/vendor/socket.io/')
+
+@sio.event(namespace='/react_ArrayOV')
+def uiBuilder(data):
+	if data['topic'].startswith('array'):
+		solarIndex = int(data['topic'][5]) - 1
+		solarProp = data['topic'][7:]
+		solarArrayData[solarIndex][solarProp] = data['payload']
+
+def init_bp(app):
 	@bp.route('/')
 	def home():
-		return render_template('solar.html')
-	
-	@bp.route('/<path:path>',methods=['GET','POST','DELETE'])
-	def proxy(path):
-		SITE_NAME = 'http://10.0.122.76:1880/react_ArrayOV/'
-		print(f'{SITE_NAME}{path}')
-		if request.method=='GET':
-			resp = requests.get(f'{SITE_NAME}{path}')
-			excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-			headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-			response = Response(resp.content, resp.status_code, headers)
-			return response
-		elif request.method=='POST':
-			resp = requests.post(f'{SITE_NAME}{path}',json=request.get_json())
-			excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-			headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-			response = Response(resp.content, resp.status_code, headers)
-			return response
-		elif request.method=='DELETE':
-			resp = requests.delete(f'{SITE_NAME}{path}').content
-			response = Response(resp.content, resp.status_code, headers)
-			return response
+		return render_template('solar.html');
+
+	@bp.route('/api')
+	def api():
+		return {
+			'solarData': solarArrayData,
+			'trackerTiltDegree': 220,
+			'azimuthAngle': 130
+		}
